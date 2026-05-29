96cf8f062c5e56f97967dd8cd8b484a17e0141b1
arun050775-code <arun050775@gmail.com>
2026-05-29 11:44:04 +0530

    fix: correct supabase anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+
 const supabaseUrl = 'https://xalbjrmridjgdpguobdx.supabase.co'
-const supabaseKey = 'sb_publishable_LuLFWWbmK8i9NQ8BV3SIZA__6Usbvfs'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhbGJqcm1yaWRqZ2RwZ3VvYmR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMDMwODEsImV4cCI6MjA5MTg3OTA4MX0.NUd_8Wm8k_xc0TfOpSkL1CcRb_C0olPUnLavLxhqjm4'
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
